8ab393bc618df93ce9de3578827c376bde544e87
mohamed23saleh <mm.hh.saleh@gmail.com>
2023-02-24 10:09:09 -0800

    m

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+int main(void)
+{
+        isUpper();
+        return(0);
+}
+int isUpper(char c){
+    if toupper(c) == c{
+        return 1;
+    }else {
+    return 0;
+    }
+}
